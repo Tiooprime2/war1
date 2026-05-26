cea131cf41e7134825f1f3dab4fdc27773e18999
Tiooprime2 <hrid9112@gmail.com>
2026-05-26 19:13:38 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -2,7 +2,7 @@
 -- ║        TIOO BETA V1 — MAIN LOADER        ║
 -- ╚══════════════════════════════════════════╝
 
-local BASE_URL = "https://raw.githubusercontent.com/Tiooprime2/Ninja-legend/refs/heads/main/"
+local BASE_URL = "https://raw.githubusercontent.com/Tiooprime2/war1/refs/heads/main/"
 
 local UI      = loadstring(game:HttpGet(BASE_URL .. "SemuaUI/ui.lua"))()
 local Main    = loadstring(game:HttpGet(BASE_URL .. "MainFeat/main_tab.lua"))()
