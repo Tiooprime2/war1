b16cd039c9db6d5455b6a08fead5b47e4f1815d6
Tiooprime2 <hrid9112@gmail.com>
2026-05-27 14:10:09 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -4,8 +4,9 @@
 
 local BASE_URL = "https://raw.githubusercontent.com/Tiooprime2/war1/refs/heads/main/"
 
-local UI     = loadstring(game:HttpGet(BASE_URL .. "ui.lua"))()
-local Hitbox = loadstring(game:HttpGet(BASE_URL .. "hitbox.lua"))()
-local Fastspeed = loadstring(game:HttpGet(BASE_URL .. "Fastspeed.lua"))()
+local UI        = loadstring(game:HttpGet(BASE_URL .. "ui.lua"))()
+local Hitbox    = loadstring(game:HttpGet(BASE_URL .. "hitbox.lua"))()
+local FastSpeed = loadstring(game:HttpGet(BASE_URL .. "FastSpeed.lua"))()
+
 Hitbox.init(UI.hitboxPage, UI.THEME, UI.tween, UI.corner, UI.stroke, UI.mainGui)
-Fastspeed.init(UI.hitboxPage, UI.THEME, UI.tween, UI.corner, UI.stroke, UI.mainGui)
+FastSpeed.build(UI.mainPage, UI)  -- ← pakai mainPage
